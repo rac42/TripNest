ec406a2b7f06fd6c80059ff5c9ea0400daf99f63
rac42 <rachit0942@gmail.com>
2024-11-15 15:52:42 +0530

    added the backend url

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://tripnest-33ea.onrender.com',
 });
 
 export default instance;
